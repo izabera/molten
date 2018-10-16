9c2c3d48f038480005d301e1d241754320d9a03c
izabera <izaberina@gmail.com>
2018-10-16 16:00:24 +0100

    basic line editing: done!

--- line.c
+++ line.c
@@ -11,92 +11,100 @@
 #include <unistd.h>
 
 
+FILE *target;
+#define print(...) fprintf(target, ## __VA_ARGS__)
+
 static struct {
   unsigned short pos;
   char data[1000];
 } buffer;
-int needtodelete;
 
 
-void backspace() { if (buffer.pos) { buffer.pos--; needtodelete = 1; } }
-void pushchar(char c) { if (buffer.pos < sizeof buffer.data - 1) buffer.data[buffer.pos++] = c; }
+static void pushchar(char c) { if (buffer.pos < sizeof buffer.data - 1) buffer.data[buffer.pos++] = c; }
 
 
 static struct winsize w;
 
 
-void display(unsigned long exp) {
+static int display_line(void) {
   int written = 0;
   int down = 0;
   while (written < buffer.pos) {
     int towrite = buffer.pos - written;
-    int space = w.ws_col - 3;
+    int space = w.ws_col - 1;
     if (space <= towrite) {
-      written += fwrite(buffer.data + written, 1, space, stdout);
-      puts("\r");
+      written += fwrite(buffer.data + written, 1, space, target);
+      print("\r\n");
       down++;
     }
     else
-      written += fwrite(buffer.data + written, 1, towrite, stdout);
+      written += fwrite(buffer.data + written, 1, towrite, target);
   }
-  putchar('_');
-  if (needtodelete) {
-    printf("\e[K\e[J");
-    needtodelete = 0;
-  }
-  if (down) printf("\e[%dA", down);
+  return down;
+}
 
+void display_cursor(unsigned long exp) {
   static unsigned long color, direction = -1;
-  printf("\r"
-         "\e[%dC"
-         "\e[48;2;%d;0;0m \e[m"
-         "\r",
-         w.ws_col - 2,
-         color);
+
+  print("\e[48;2;%d;0;0m \e[m", color);
+  print("\e[K\e[J");
+
   if (color <= 0 || color >= 255)
     direction *= -1;
   color += direction * exp * 5;
+}
 
-  fflush(stdout);
+void display(unsigned long exp) {
+  int down = display_line();
+
+  display_cursor(exp);
+
+  if (down) print("\e[%dA", down);
+
+  print("\r");
+  fflush(target);
 }
 
 
 static struct termios oldterm, newterm;
 
 void quit(int sig) {
-  printf("\e[m\e[?25h\n");
+  print("\e[m\e[K\e[J\e[?25h");
   tcsetattr(0, TCSANOW, &oldterm);
-  exit(0);
+  fwrite(buffer.data, 1, buffer.pos, stdout);
+  printf("\n");
+  exit(sig ? 128+sig : 0);
 }
 
 
 static void ctrl(char c) {
   switch (CTRL(c)) {
     case CTRL('C'):
-      quit(0);
+      quit(2);
       break;
+    case CTRL('D'):
     case CTRL('J'):
     case CTRL('M'):
-      printf("\r\n");
-      ctrl('U');
+      quit(0);
       break;
     case CTRL('?'):
     case CTRL('H'):
-      if (buffer.pos) {
+      if (buffer.pos)
         buffer.pos--;
-        needtodelete = 1;
-      }
       break;
     case CTRL('U'):
       buffer.pos = 0;
-      needtodelete = 1;
       break;
     case CTRL('W'):
+      while (buffer.pos && buffer.data[buffer.pos-1] == ' ') buffer.pos--;
+      while (buffer.pos && buffer.data[buffer.pos-1] != ' ') buffer.pos--;
   }
 }
 
 
 int main() {
+  target = stderr;
+
   tcgetattr(0, &oldterm);
   cfmakeraw(&newterm);
   tcsetattr(0, TCSANOW, &newterm);
@@ -105,21 +113,21 @@ int main() {
     struct rgb { int r, g, b; } fg, bg;
     char buf[50] = { };
 
-    printf("\e]11;?\e\\");
-    fflush(stdout);
+    print("\e]11;?\e\\");
+    fflush(target);
     read(0, buf, sizeof buf);
     sscanf(buf, "\e]11;rgb:%o/%o/%o", &fg.r, &fg.g, &fg.b);
 
-    printf("\e]10;?\e\\");
-    fflush(stdout);
+    print("\e]10;?\e\\");
+    fflush(target);
     read(0, buf, sizeof buf);
     sscanf(buf, "\e]11;rgb:%o/%o/%o", &fg.r, &fg.g, &fg.b);
   }
 
 
-  setvbuf(stdout, NULL, _IOFBF, 0); // stupid libc, let me handle this
-  printf("\e[m\e[?25l");
-  fflush(stdout);
+  setvbuf(target, NULL, _IOFBF, 0); // stupid libc, let me handle this
+  print("\e[m\e[?25l");
+  fflush(target);
 
 
   ioctl(0, TIOCGWINSZ, &w);
@@ -172,7 +180,6 @@ int main() {
       struct signalfd_siginfo si;
       read(sigfd, &si, sizeof si);
       ioctl(0, TIOCGWINSZ, &w);
-      needtodelete = 1;
     }
     if (FD_ISSET(timerfd, &fds)) {
       unsigned long exp;
